c7fbba3ab227a6402f39aa22479a90be1d773c0d
JonLec2 <92182897+JonLec2@users.noreply.github.com>
2022-01-26 12:55:15 -0600

    Add files via upload

---
+++ Controles.js
@@ -0,0 +1,81 @@
+class Controles{
+    constructor(){
+      
+        this.button2=createButton('⊗')
+        this.button3=createButton('▲')
+
+    }
+
+    hide(){
+      this.button2.hide();
+        this.button3.hide();
+      }
+
+display(){
+    
+
+   
+ this.button2.position(width-60 , height/2-70);
+ this.button2.style('background', '#FBBB21');
+ this.button2.style('font-size', '60px');
+ this.button2.style('color', 'white');
+ this.button2.style('border-radius', '60px');
+
+ this.button3.position(50 , height/2-30);
+ this.button3.style('background', '#FBBB21');
+ this.button3.style('font-size', '60px');
+ this.button3.style('color', 'white');
+ this.button3.style('border-radius', '60px');
+
+
+
+
+
+
+
+this.button2.touchStarted(()=>{
+    if(spider.x>4800 && spider.x<6000){
+    laserultimo=createSprite(spider.x, spider.y, 40, 10)
+    laserultimo.addImage(laserultimo2Img)
+    laserultimo.scale=0.1
+    laserultimo.velocityX=+17
+    laserultimo.lifetime=100
+    laserultimoGroup.add(laserultimo)
+    }
+
+    if(spider.x>6025){
+    laserultimo2=createSprite(spider.x, spider.y, 40, 10)
+ laserultimo2.addImage(laserultimo2Img)
+ laserultimo2.scale=0.1
+    laserultimo2.velocityX=+17
+    laserultimo2.lifetime=100
+  laserultimo2Group.add(laserultimo2)
+    }
+
+
+return false;
+
+})
+
+
+this.button3.touchStarted(()=>{
+    if(spider.y>300){
+    spider.velocityY=spider.velocityY-31
+    spider.maxSpeed=+15
+    }
+
+    else{
+        spider.velocityY=spider.velocityY-0
+        spider.maxSpeed=+15
+
+    }
+    return false;
+
+})
+
+
+
+
+}
+}
+
